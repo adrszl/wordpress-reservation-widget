fff625274895d61cedc91b8c6de8c1daedaefc5c
Adrian Szlegel <adrian.szlegel@hotmail.com>
2022-07-25 21:11:42 +0200

    skeleton added

---
+++ wordpress-reservation-widget.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Plugin Name: Wordpress Reservation Widget
+ * Description: Plugin with functionality to add reservation widget
+ * Version: 0.0.1
+ * Author: Adrian Szlegel
+ */
+
+
+// MAIN PLUGIN DIRECTORY
+require_once(dirname(__FILE__).'/widget/reservation-plugin.php');
